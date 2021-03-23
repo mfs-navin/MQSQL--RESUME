3adf059e57af40e8a75661f7766509cd55d823a7
mfs-navin <78721702+mfs-navin@users.noreply.github.com>
2021-03-23 12:07:16 +0530

    modified sql to use separate table for skills

--- php/output.php
+++ php/output.php
@@ -1,58 +1,10 @@
 <?php
-	session_start();
+session_start();
 
-if(isset($_SESSION["name"]))
+if(isset($_SESSION["email"]))
 {
-///Variables to store session data
-    $name = $_SESSION["name"];
-    $gender = $_SESSION["gender"];
-    $email = $_SESSION["email"];
-    $phone = $_SESSION["phone"];
-    $skills = $_SESSION["skills"];
-    $photo = $_SESSION["profile_pic"];
-    $about = $_SESSION["about"];
-    $address = $_SESSION["addr"];
-    $education = $_SESSION["education"];
-    $linkedin = $_SESSION["linkedin"];
-    $github = $_SESSION["github"];
 
-    $db_skills = '';
-
-/// Variable to connect to the database
-	$servername = "localhost";
-	$username = "username";
-	$password = "password";
-	$dbname = "myDB";    
-
-
-///Displaying the data entered by the user using session data
-    echo "Your inputs:". "<br />";
-	echo "-------------------------------------". "<br />";
-	echo "Name: " . $name . "<br />";
-	echo "Gender:";
-	if ($gender=='m') {
-		echo "Male" . "<br/>";
-	}
-	else{
-		echo "Female" . "<br/>";
-	}
-	echo "Email: " . $email . "<br />";
-	echo "Phone: " . $phone . "<br />";
-	echo "Skill: " ;
-	foreach ($skills as $key => $value) {
-		echo  $value .", ";
-		$db_skills = $db_skills . $value . ', ';
-	}
-	echo "<br/>";
-	echo "Photo: " . $photo ."<br/>";
-	echo '<img src="../upload/'.$photo .'" alt="Random image" />'."<br /><br />";
-	echo "About: " . $about . "<br />";
-	echo "Address: " . $address . "<br />";
-	echo "Education Qualification: " . $education . "<br />";
-	echo "Linkedin url: ". $linkedin. "<br/>";
-	echo "Github url: ". $github. "<br/>";
-
-
+	$email = $_SESSION["email"];
 
 /// Connecting database
 	$servername = "localhost";
@@ -60,34 +12,105 @@ if(isset($_SESSION["name"]))
 	$password = "mindfire";
 	$dbname = "user_resume";
 
-// Create connection
+  // Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-	if ($conn->connect_error) {
-	  die("Connection failed: " . $conn->connect_error);
+  // Check connection
+	if ($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
 	}
 
-	$sql = "INSERT INTO resume_data (id, name, gender, email, contact_number, skills, photo, about, address, edu_qualification, linkedin, github) VALUES (NULL, '$name', '$gender', '$email', '$phone', '$db_skills', '$photo', '$about', '$address', '$education', '$linkedin', '$github')";
+	$sql_resume_fetch = "SELECT id FROM resume WHERE email = '$email'";
 
-	if ($conn->query($sql) === TRUE) {
-		
-/// Successfull insertion of data
-	  echo "New record created successfully";
+	if ($resume_record = $conn->query($sql_resume_fetch) === TRUE) {
+
+ /// Successfull insertion of data
+		echo "Record fetched successfully from resume table";
+	} 
+
+	else 
+	{
+
+ /// Error fetching data
+		echo "Error: " . $sql_resume_fetch . "<br>" . $conn->error;
+	}
+
+	$user_id = $resume_record->fetch_assoc()['id'];
+
+
+	$sql_ouput = "SELECT * FROM resume WHERE email = '$email' ";
+
+	if ($resume = $conn->query($sql_ouput) === TRUE) {
+
+  /// Successfull insertion of data
+		echo "Record fetched successfully";
+
 	} 
 
 	else {
 
-/// Error inserting data
-	  echo "Error: " . $sql . "<br>" . $conn->error;
+  /// Error inserting data
+		echo "Error: " . $sql_output . "<br>" . $conn->error;
 	}
+
+
+	$sql_skills = "SELECT user_skills.user_id , user_skills.skill_id, skills.skill FROM user_skills INNER JOIN skills ON user_skills.skill_id = skills.id WHERE user_skills.user_id = '$user_id' ";
+
+	if ($skills = $conn->query($sql_ouput) === TRUE) {
+
+  /// Successfull insertion of data
+		echo "Record fetched successfully";
+
+	} 
+
+	else {
+
+  /// Error inserting data
+		echo "Error: " . $sql_skills . "<br>" . $conn->error;
+	}
+
+	if ($resume->num_rows > 0 && $skills->num_rows > 0) 
+	{
+		$resume_row = $resume->fetch_assoc();
+
+///Displaying the data entered by the user using session data
+		echo "Your inputs:". "<br />";
+		echo "-------------------------------------". "<br />";
+		echo "Name: " . $resume_row['name'] . "<br />";
+		echo "Gender:";
+		if ($resume_row['gender']==='m') {
+			echo "Male" . "<br/>";
+		}
+		else{
+			echo "Female" . "<br/>";
+		}
+		echo "Email: " . $resume_row['email'] . "<br />";
+		echo "Phone: " . $resume_row['contact_number'] . "<br />";
+		echo "Skill: " ;
+		while($row= $skills->fetch_assoc())
+		{
+			$row['skill'] . ", ";
+		}
+		echo "<br/>";
+		echo "Photo: " . $resume_row['photo'] ."<br/>";
+		echo '<img src="../upload/'.$resume_row['photo'] .'" alt="Random image" />'."<br /><br />";
+		echo "About: " . $resume_row['about'] . "<br />";
+		echo "Address: " . $resume_row['address'] . "<br />";
+		echo "Education Qualification: " . $resume_row['edu_qualification'] . "<br />";
+		echo "Linkedin url: ". $resume_row['linkedin']. "<br/>";
+		echo "Github url: ". $resume_row['github']. "<br/>";
+
+	}
+
+
 
 	$conn->close();
 
 
-// Destroying session
-	session_unset();
-	session_destroy();
+// // Destroying session
+// 	session_unset();
+// 	session_destroy();
 }
 
 else{
